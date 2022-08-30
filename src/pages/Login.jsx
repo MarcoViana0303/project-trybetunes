4c58cfb97def5afea0a8e7ae7de6609d9f02c1ea
Marco Viana <marcoviana100@gmail.com>
2022-08-30 18:34:28 -0300

    Projeto Iniciado, vamo com tudo!

---
+++ src/pages/Login.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+export default class Login extends React.Component {
+    render() {
+        return (
+            <div data-testid="page-login">
+
+            <h1>Oieeeeeeeee</h1>
+
+                
+            </div>
+
+        )
+    }
+}
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class Profile extends React.Component {
+    render() {
+        return (
+            <div data-testid="page-profile">
+
+
+
+            </div>
+
+        )
+    }
+}
