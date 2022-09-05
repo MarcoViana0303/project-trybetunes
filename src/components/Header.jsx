158713e587fe2bfa72c6776578a673d96ec34627
Marco Viana <marcoviana100@gmail.com>
2022-09-05 18:59:10 -0300

    Req 5

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,65 +3,60 @@ import React from 'react';
 
 import { getUser } from '../services/userAPI';
 
-import Search from '../pages/Search';
-
 export default class Header extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            loading: false,
-            name: '',
-            aparecerNome: false,
-        }
-    }
-    componentDidMount() {
-        this.carregarHeader();
-        
-    }
-    
-    carregarHeader = async () => {
-        this.setState({
-            loading: true,
-        })
-        const user = await getUser()
-        this.setState({
-            loading: false,
-            aparecerNome: true,
-        })
-        const nome= user.name;
-        this.setState({
-            name: nome,
-        })
-}
-    render() {
-        const { loading, name, aparecerNome } = this.state;
-        return (
-            <header data-testid="header-component">
-                <div>
-                    <Link data-testid="link-to-search" to="/search">Search</Link>
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      name: '',
+      aparecerNome: false,
+    };
+  }
 
-                </div>
+  componentDidMount() {
+    this.carregarHeader();
+  }
 
-                <div >
-                    <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+  carregarHeader = async () => {
+    this.setState({
+      loading: true,
+    });
+    const user = await getUser();
+    this.setState({
+      loading: false,
+      aparecerNome: true,
+    });
+    const nome = user.name;
+    this.setState({
+      name: nome,
+    });
+  };
 
-                </div>
-                <div >
-                    <Link data-testid="link-to-profile" to="/profile">Profile</Link>
-                </div>
-                <div data-testid="header-user-name">
-                
-            
-                {loading && <p>Carregando...</p> }
-                {aparecerNome && <p>{name}</p>}
+  render() {
+    const { loading, name, aparecerNome } = this.state;
+    return (
+      <header data-testid="header-component">
+        <div>
+          <Link data-testid="link-to-search" to="/search">Search</Link>
 
-            
+        </div>
 
-                </div>
+        <div>
+          <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
 
+        </div>
+        <div>
+          <Link data-testid="link-to-profile" to="/profile">Profile</Link>
+        </div>
+        <div data-testid="header-user-name">
 
-            </header>
+          {loading && <p>Carregando...</p> }
+          {aparecerNome && <p>{name}</p>}
 
-        );
-    }
+        </div>
+
+      </header>
+
+    );
+  }
 }
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,12 +1,56 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 export default class Search extends React.Component {
+  state = {
+    bandaArt: '',
+    disable: true,
+  };
+
+  ativarBotao = () => {
+    const { bandaArt } = this.state;
+    const threeCharacter = 2;
+    const validar = bandaArt.length >= threeCharacter;
+    this.setState({
+      disable: !validar,
+    });
+  };
+
+  onInput = ({ target }) => {
+    const { name } = target;
+    const valor = target.value;
+    this.setState({
+      [name]: valor,
+    }, () => {
+      this.ativarBotao();
+    });
+  };
+
   render() {
+    const { disable, bandaArt } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            name="bandaArt"
+            onChange={ this.onInput }
+            value={ bandaArt }
+            placeholder="Artista/Banda"
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ disable }
+            onClick={ this.ativarBotao }
+          >
+            Pesquisar
+          </button>
+
+        </form>
       </div>
 
     );
