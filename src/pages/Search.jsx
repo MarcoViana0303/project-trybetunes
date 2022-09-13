0359a4d47714004ee115123557c7e4fceb4a5d22
Marco Viana <marcoviana100@gmail.com>
2022-09-12 22:26:13 -0300

    REQUISITO 6 - CONCLUÍDO 2.0

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 export default class Search extends React.Component {
   state = {
     bandaArt: '',
     disable: true,
+    loading: false,
+    cantor: [],
+    bandaArtInput: '',
   };
 
-  ativarBotao = () => {
-    const { bandaArt } = this.state;
-    const threeCharacter = 2;
-    const validar = bandaArt.length >= threeCharacter;
-    this.setState({
-      disable: !validar,
+  ativarBotao = (param) => {
+    this.setState(({
+      loading: true,
+      bandaArt: '',
+    }), async () => {
+      const artist = await searchAlbumsAPI(param);
+      if (artist) {
+        this.setState(({
+          loading: false,
+          cantor: artist || [],
+          bandaArtInput: param,
+        }));
+      }
     });
   };
 
@@ -22,35 +34,61 @@ export default class Search extends React.Component {
     this.setState({
       [name]: valor,
     }, () => {
-      this.ativarBotao();
+      const { bandaArt } = this.state;
+      const threeCharacter = 2;
+      const validar = bandaArt.length >= threeCharacter;
+      this.setState({
+        disable: !validar,
+
+      });
     });
   };
 
   render() {
-    const { disable, bandaArt } = this.state;
+    const { disable, bandaArt, loading, cantor, bandaArtInput } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <form>
 
-          <input
-            type="text"
-            data-testid="search-artist-input"
-            name="bandaArt"
-            onChange={ this.onInput }
-            value={ bandaArt }
-            placeholder="Artista/Banda"
-          />
-          <button
-            type="button"
-            data-testid="search-artist-button"
-            disabled={ disable }
-            onClick={ this.ativarBotao }
-          >
-            Pesquisar
-          </button>
+        {loading ? <p>Carregando...</p>
+          : <form>
 
-        </form>
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              name="bandaArt"
+              onChange={ this.onInput }
+              value={ bandaArt }
+              placeholder="Artista/Banda"
+            />
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ disable }
+              onClick={ () => { this.ativarBotao(bandaArt); } }
+            >
+              Pesquisar
+            </button>
+          </form>}
+        {cantor.length > 0 && <h1>{`Resultado de álbuns de: ${bandaArtInput}`}</h1>}
+        {cantor.length === 0 && 'Nenhum álbum foi encontrado'}
+
+        {cantor.map((elemento) => (
+          <div key={ elemento.collectionId }>
+
+            <img src={ elemento.artworkUrl100 } alt={ elemento.artistId } />
+            <p>{elemento.artistName}</p>
+            <p>{elemento.collectionName}</p>
+            <Link
+              to={ `/album/${elemento.collectionId}` }
+              data-testid={ `link-to-album-${elemento.collectionId}` }
+            >
+              Link
+            </Link>
+          </div>
+
+        ))}
+
       </div>
 
     );
